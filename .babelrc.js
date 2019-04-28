dcd0f6e16e51cacf705427c2014cd44b2c9deb3a
潘志坚 <jim.pan@dji.com>
2019-04-29 01:59:05 +0800

    script

--- .babelrc.js
+++ .babelrc.js
@@ -1,12 +1,25 @@
 module.exports = {
-  "presets": ["@babel/preset-env", "@babel/preset-react"],
+  "presets": [
+    ["@babel/env", {
+      "targets": {
+        "browsers": [
+          "last 2 versions",
+          "> 5%"
+        ]
+      },
+      "useBuiltIns": "usage",
+      "modules": process.env.BABEL_MODULES || "commonjs" // babel's default is commonjs
+    }],
+    "@babel/typescript",
+    "@babel/react"
+  ],
   "plugins": [
     "@babel/plugin-transform-runtime",
     "@babel/plugin-proposal-class-properties",
     "babel-plugin-add-react-displayname"
   ],
   "env": {
-    "esm": {
+    "es": {
       "presets": [["@babel/preset-env", { "modules": false }]],
       "plugins": [["@babel/plugin-transform-runtime", { "useESModules": true }]]
     }
